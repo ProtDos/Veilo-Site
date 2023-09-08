934dd7a70ccde219c60bc0493db4f0d7ee757f96
ProtDos <69071809+ProtDos@users.noreply.github.com>
2023-09-08 20:42:07 +0200

    Add files via upload

---
+++ js/landing/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $.cookit({
+        messageColor: '#fff',
+        linkColor: 'var(--primary-color)',
+        buttonColor: 'var(--primary-color)',
+        messageText: "Veilo uses  <b> cookies </b> to ensure you get the best experience on our website.",
+        linkText: "Learn more",
+        linkUrl: "cookies_policy.html",
+        buttonText: "I accept",
+    });
+});
