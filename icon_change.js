b5b8edceaccb4693c1f6f1b8d7f28b971306ee53
ProtDos <elku001@yahoo.com>
2023-12-15 20:49:14 +0100

    fixed burger / links / text

--- icon_change.js
+++ icon_change.js
@@ -1,37 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    const changeFavicon = () => {
-        const favicon = document.getElementById('favicon');
-        if (darkModeQuery.matches) {
-            favicon.href = 'media/logo/loggi.png';
-        } else {
-            favicon.href = 'media/logo/image2vector.svg';
-        }
-    };
-    
-    changeFavicon();
-    darkModeQuery.addEventListener('change', changeFavicon);
-});
+let pressed = false;
 
-document.addEventListener('DOMContentLoaded', function () {
-    let pressed = false;
-    const burger = document.querySelector('.burger');
-
-    burger.addEventListener('click', () => {
-        burger.classList.toggle('active');
-        if (pressed == false) {
-        document.getElementById('overlay').classList.remove('hidden');
-        pressed = true;
-        } else { 
-        document.getElementById('overlay').classList.add('hidden');
-        pressed = false;
-        }
-        
-    });
-
-    function closeBurger() {
-        burger.classList.toggle('active');
-        document.getElementById('overlay').classList.add('hidden');
-        pressed = false;
+const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+const changeFavicon = () => {
+    const favicon = document.getElementById('favicon');
+    if (darkModeQuery.matches) {
+        favicon.href = 'media/logo/loggi.png';
+    } else {
+        favicon.href = 'media/logo/image2vector.svg';
     }
+};
+
+changeFavicon();
+darkModeQuery.addEventListener('change', changeFavicon);
+
+const burger = document.querySelector('.burger');
+
+console.log("loaded");
+
+burger.addEventListener('click', () => {
+    console.log("toggled");
+    burger.classList.toggle('active');
+    if (pressed == false) {
+    document.getElementById('overlay').classList.remove('hidden');
+    pressed = true;
+    } else { 
+    document.getElementById('overlay').classList.add('hidden');
+    pressed = false;
+    }
+    
 });
+
+function closeBurger() {
+    console.log("closed");
+    burger.classList.toggle('active');
+    document.getElementById('overlay').classList.add('hidden');
+    pressed = false;
+}
